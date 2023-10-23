7c396306ba3d06d5122517146953455f8a08dd49
AndrewArcorp <andrew.maher@egyptianservicecard.com>
2023-10-23 15:20:25 +0300

    PSD-37 Fix Price need to be in these tables

--- src/main/java/com/aman/payment/maazoun/mapper/StockLabelMapper.java
+++ src/main/java/com/aman/payment/maazoun/mapper/StockLabelMapper.java
@@ -2,8 +2,11 @@ package com.aman.payment.maazoun.mapper;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.aman.payment.auth.model.SubServicePriceTier;
+import com.aman.payment.auth.service.SubServicePriceTierService;
 import org.springframework.stereotype.Service;
 
 import com.aman.payment.maazoun.model.MaazounBookStockLabel;
@@ -13,39 +16,47 @@ import com.aman.payment.util.Util;
 @Service
 public class StockLabelMapper {
 
-	/*
-	 * MaazounBookStockLabel to StockLabelDTO
-	 * MaazounBookStockLabels to StockLabelDTOs
-	 * ****************************************************************************************************
-	 */
-	public StockLabelDTO maazounBookStockLabelToStockLabelDTO(MaazounBookStockLabel maazounBookStockLabel) {
-		if (maazounBookStockLabel!=null) {
-	        return createStockLabelDTO(maazounBookStockLabel);
+    final SubServicePriceTierService subServicePriceTierService;
 
-		}
-		else return null ;
+    public StockLabelMapper(SubServicePriceTierService subServicePriceTierService) {
+        this.subServicePriceTierService = subServicePriceTierService;
     }
-	
-	public List<StockLabelDTO> maazounBookStockLabelsToStockLabelDTOs(List<MaazounBookStockLabel> maazounBookStockLabels) {
-		return maazounBookStockLabels.stream().filter(Objects::nonNull)
-				.map(this::maazounBookStockLabelToStockLabelDTO).collect(Collectors.toList());
-	}
-	
-	private StockLabelDTO createStockLabelDTO(MaazounBookStockLabel maazounBookStockLabel) {
-		StockLabelDTO stockLabelDTO = new StockLabelDTO();
-		stockLabelDTO.setCreatedAt(Util.dateFormatReport(maazounBookStockLabel.getCreatedAt()));
-		stockLabelDTO.setCreatedBy(maazounBookStockLabel.getCreatedBy());
-		stockLabelDTO.setStockLabelId(maazounBookStockLabel.getId());
-		stockLabelDTO.setLabelCode(maazounBookStockLabel.getLabelCode());
-		stockLabelDTO.setLocationId(maazounBookStockLabel.getLocationId());
-		stockLabelDTO.setStatusFk(maazounBookStockLabel.getStatusFk());
-		stockLabelDTO.setBookTypeId(String.valueOf(maazounBookStockLabel.getBookTypeId()));
-		if(maazounBookStockLabel.getUpdatedAt() != null)
-			stockLabelDTO.setUpdatedAt(Util.dateFormatReport(maazounBookStockLabel.getUpdatedAt()));
-		stockLabelDTO.setUpdatedBy(maazounBookStockLabel.getUpdatedBy());
-		
-		return stockLabelDTO;
-	}
-	
+
+    /*
+     * MaazounBookStockLabel to StockLabelDTO
+     * MaazounBookStockLabels to StockLabelDTOs
+     * ****************************************************************************************************
+     */
+    public StockLabelDTO maazounBookStockLabelToStockLabelDTO(MaazounBookStockLabel maazounBookStockLabel) {
+        if (maazounBookStockLabel != null) {
+            return createStockLabelDTO(maazounBookStockLabel);
+
+        } else return null;
+    }
+
+    public List<StockLabelDTO> maazounBookStockLabelsToStockLabelDTOs(List<MaazounBookStockLabel> maazounBookStockLabels) {
+        return maazounBookStockLabels.stream().filter(Objects::nonNull)
+                .map(this::maazounBookStockLabelToStockLabelDTO).collect(Collectors.toList());
+    }
+
+    private StockLabelDTO createStockLabelDTO(MaazounBookStockLabel maazounBookStockLabel) {
+        StockLabelDTO stockLabelDTO = new StockLabelDTO();
+        stockLabelDTO.setCreatedAt(Util.dateFormatReport(maazounBookStockLabel.getCreatedAt()));
+        stockLabelDTO.setCreatedBy(maazounBookStockLabel.getCreatedBy());
+        stockLabelDTO.setStockLabelId(maazounBookStockLabel.getId());
+        stockLabelDTO.setLabelCode(maazounBookStockLabel.getLabelCode());
+        stockLabelDTO.setLocationId(maazounBookStockLabel.getLocationId());
+        stockLabelDTO.setStatusFk(maazounBookStockLabel.getStatusFk());
+        stockLabelDTO.setBookTypeId(String.valueOf(maazounBookStockLabel.getBookTypeId()));
+        if (maazounBookStockLabel.getUpdatedAt() != null)
+            stockLabelDTO.setUpdatedAt(Util.dateFormatReport(maazounBookStockLabel.getUpdatedAt()));
+        stockLabelDTO.setUpdatedBy(maazounBookStockLabel.getUpdatedBy());
+
+        Optional<SubServicePriceTier> subServicePriceTier = subServicePriceTierService.findById(maazounBookStockLabel.getBookTierId());
+        subServicePriceTier.ifPresent(servicePriceTier -> stockLabelDTO.setBookTierFees(servicePriceTier.getFees()));
+
+        return stockLabelDTO;
+    }
+
 
 }
